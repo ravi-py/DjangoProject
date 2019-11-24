97893b9bf8e5a994db8c9353465c89f6a031e648
ravi <ravia9134.gmail.com>
2019-11-24 16:28:30 +0530

    notes_project

---
+++ notes_project/notes/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from . import models
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields=('id','title','description','created_at','updated_at')
+        model=models.Note
---
+++ notes_project/notes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+
+# Create your views here.
+class NoteList(generics.ListAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class =NoteSerializer
